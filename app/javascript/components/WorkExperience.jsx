389b51bab000796a44f054b49aa10d29663163b9
Seba <dj.sebamatzzo@gmail.com>
2021-09-23 21:51:38 +1000

    Work experience starting to display inside Info Sections

---
+++ app/javascript/components/WorkExperience.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export class WorkExperience extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
